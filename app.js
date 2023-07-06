6919fefa97a5bca81ecd946543462ced0d04fbed
Braian-Quintian <bquintianromero@gmail.com>
2023-07-06 14:26:22 -0500

    feat: :sparkles: COMPUESTO

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
 import { createServer } from 'http';
-import fetch from 'node-fetch';
-
-const appHttp = createServer(async (req, res) => {
-  if (req.url === '/') {
-    try {
-      const response = await fetch('http://127.127.127.127:3000/index.html');
-      const data = await response.text();
-      console.log(data);
-      res.end('Hola');
-    } catch (error) {
-      console.error(error);
-      res.end('Error al obtener los datos');
-    }
-  } else {
-    res.end('Ruta no encontrada');
-  }
-});
-
+import fs from 'fs';
+import https from 'https';
 const config = {
-  hostname: '127.127.127.127',
+  hostname: 'nasa.com',
   port: 3000
 };
+const apiKey = 'FkIw9aQ7EJrI27ZoSRVSNvn5Djo4RYYoRw6iRmhZ';
+const startDate = '2023-07-01';
+const endDate = '2023-07-04';
+const appHttp = createServer((req, res) => {
+  req.url === '/asteroids'
+    ? fetchAsteroidsData()
+      .then(asteroids => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(asteroids));
+      })
+      .catch(() => res.end('Error al obtener los datos de los asteroides'))
+    : fs.readFile(`.${req.url}`, 'utf-8', (error, content) => {
+      error ? res.end('Error al obtener los datos') : res.end(content);
+    });
+});
+const fetchAsteroidsData = () =>
+  new Promise(resolve => {
+    const apiUrl = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`;
+    https.get(apiUrl, response => {
+      let data = '';
 
+      response.on('data', chunk => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+    });
+  }).then(asteroidData => extractAsteroids(asteroidData));
+const extractAsteroids = asteroidData => {
+  return Object.values(asteroidData.near_earth_objects).flatMap(dateAsteroids =>
+    dateAsteroids.map(asteroid => {
+      const { name, estimated_diameter, close_approach_data } = asteroid;
+      const size = estimated_diameter.kilometers.estimated_diameter_max;
+      const velocity = close_approach_data[0].relative_velocity.kilometers_per_hour;
+      const approachDate = close_approach_data[0].close_approach_date;
+      return { name, size, velocity, approachDate };
+    })
+  );
+};
 appHttp.listen(config.port, config.hostname, () => {
-  console.log(`Server running at http://${config.hostname}:${config.port}`);
+  console.log(`Servidor en ejecución en http://${config.hostname}:${config.port}`);
 });
